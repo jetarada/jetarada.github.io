1a98991b608c80a771efc4d5147aaa673db1e8db
jetarada <midorichanxp@gmail.com>
2024-11-01 13:38:11 +0800

    page updated | right-click disable

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// Disable right-click
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
-  });
-  
+});
+
+// Disable Ctrl+U
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+        // alert("Viewing source is disabled.");
+    }
+});
